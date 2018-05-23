d9ec1e02e1569f7a22f1d600dcb3986c2b0d7432
yungshenglu <yungshenglu1994@gmail.com>
2018-05-23 13:36:46 +0800

    ZOJ-a006 accepted

--- ZOJ-a006/ZOJ-a006.cpp
+++ ZOJ-a006/ZOJ-a006.cpp
@@ -17,11 +17,13 @@ int main(void) {
         if (det < 0)
             printf("No real root\n");
         else {
+            // Calculate the first of root
             double root1 = (-b + sqrt(det)) / (2 * a);
 
             if (det == 0)
                 printf("Two same roots x=%d\n", int(root1));
             else {
+                // Calculate the seconf of root
                 double root2 = (-b - sqrt(det)) / (2 * a);                
                 printf("Two different roots x1=%d , x2=%d\n", int(root1), int(root2));
             }
