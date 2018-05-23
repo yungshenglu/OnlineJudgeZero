b14190d640b404d9072e4d65d28a5afca24e3aa3
yungshenglu <yungshenglu1994@gmail.com>
2018-05-23 09:43:43 +0800

    ZOJ-a001 accepted

---
+++ ZOJ-a001/ZOJ-a001.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string text;
+
+    while (cin >> text) {
+        // Print the result
+        cout << "hello, " << text << endl;
+    }
+
+    return 0;
+}
