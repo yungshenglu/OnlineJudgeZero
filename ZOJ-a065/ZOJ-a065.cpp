8bd1501ee5ded87c94e9770b3c49a4f936066c7a
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-09-05 10:17:55 +0800

    ZOJ-a065 accepted

---
+++ ZOJ-a065/ZOJ-a065.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#define MAX 7
+
+using namespace std;
+
+int main(void) {
+    string word;
+    while (cin >> word) {
+        int password[6] = { 0 };
+        for (int i = 0; i < MAX - 1; ++i)
+            printf("%d", (word[i + 1] >= word[i]) ? word[i + 1] - word[i] : word[i] - word[i + 1]);
+        puts("");
+    }
+
+    return 0;
+} 
